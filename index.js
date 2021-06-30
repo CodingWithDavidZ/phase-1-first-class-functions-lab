bcfb3f49397a544dd714df4049591c4959fe7ee2
David Zombar <davidzombar@gmail.com>
2021-06-30 14:25:58 -0600

    commit tree 05900322a5d80d3dd13e584087af1b62f9c50eaa

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+const drivers = ["Sally", "Bob", "Freddy", "Claudia"];
+//return first 2 drivers (Sally, Bob)
+const returnFirstTwoDrivers = function (drivers) {
+  return drivers.slice(0, 2);
+};
+
+//return last 2 drivers (Freddy, Claudia)
+const returnLastTwoDrivers = function (drivers) {
+  return drivers.slice(-2);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function (multiplyFare) {
+  return function (int) {
+    return multiplyFare * int;
+  };
+};
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, choseOne) {
+  return choseOne(drivers);
+}
